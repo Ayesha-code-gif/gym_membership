b8f0a53190b76a353573cff8906c41cdf7e7faa9
Ayesha-code-gif <ayeshaimran1324@gmail.com>
2024-12-16 20:54:46 +0500

    Create function2.cpp
    this is the function that is being used to display all the details of the user on the screen.

---
+++ function2.cpp
@@ -0,0 +1,10 @@
+void displayUserDetails(int index) {
+    cout << "\nUser Details:" << endl;
+    cout << "Name: " << name[index] << endl;
+    cout << "Gender: " << gender[index] << endl;
+    cout << "CNIC: " << cnic[index] << endl;
+    cout << "Age: " << age[index] << endl;
+    cout << "Contact Number: " << contact[index] << endl;
+    cout << "Membership Plan: " << membershipPlan[index] << endl;
+    cout << "Payment Preference: " << paymentPreference[index] << endl;
+}
